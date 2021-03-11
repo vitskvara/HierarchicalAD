0f3e00eb4741fe40bc7b8265353b46816b54c1b7
DrWatson <no@mail>
2021-03-11 15:05:30 +0100

    morpho mnist labels

---
+++ scripts/morpho_mnist_labels.jl
@@ -0,0 +1,43 @@
+using DrWatson
+@quickactivate
+using HierarchicalAD
+using MLDatasets
+using FileIO, BSON
+using CSV
+using DataFrames
+
+#
+y_tr = MLDatasets.MNIST.trainlabels();
+inpath = datadir("morpho_mnist/original")
+f_tr = joinpath(inpath, "train-morpho.csv")
+label_df = CSV.read(f_tr, DataFrame)
+label_df[!,:digit] = y_tr
+CSV.write(datadir("morpho_mnist/labels.csv"), label_df)
+
+#
+bwl = load(datadir("digits_bw/labels.bson"))[:labels]
+bw_df = DataFrame(
+		:digit => [l[3] for l in bwl],
+		:font => [l[1] for l in bwl],
+		:boldness => [l[2] for l in bwl],
+		:rotation => [l[4] for l in bwl],
+		:xshear => [l[5] for l in bwl],
+		:yshear => [l[6] for l in bwl],
+		:scale => [l[7] for l in bwl]
+	)
+CSV.write(datadir("digits_bw/labels.csv"), bw_df)
+
+# RGB
+rgbl = load(datadir("digits_rgb/labels.bson"))[:labels]
+rgb_df = DataFrame(
+		:digit => [l[3] for l in rgbl],
+		:color => [l[4] for l in rgbl],
+		:font => [l[1] for l in rgbl],
+		:boldness => [l[2] for l in rgbl],
+		:rotation => [l[5] for l in rgbl],
+		:xshear => [l[6] for l in rgbl],
+		:yshear => [l[7] for l in rgbl],
+		:scale => [l[8] for l in rgbl]
+	)
+CSV.write(datadir("digits_rgb/labels.csv"), rgb_df)
+
